f0135718a1790bc2eb65a6d20960bcc834b53bf0
sanjay1982 <sanjay1982@gmial.com>
2019-12-12 21:56:08 +1100

    new mini proj

---
+++ FirstEng/Components/Quad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using Veldrid;
+
+
+namespace FirstEng.Components
+{
+public struct VertexPositionColor
+{
+    public Vector2 Position; // This is the position, in normalized device coordinates.
+    public RgbaFloat Color; // This is the color of the vertex.
+    public VertexPositionColor(Vector2 position, RgbaFloat color)
+    {
+        Position = position;
+        Color = color;
+    }
+    public const uint SizeInBytes = 24;
+}
+    public class Quad
+    {
+        public VertexPositionColor[] quadVertices =
+            {
+                new VertexPositionColor(new Vector2(-.75f, .75f), RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(.75f, .75f), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-.75f, -.75f), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(.75f, -.75f), RgbaFloat.Yellow)
+            };
+    }
+}
